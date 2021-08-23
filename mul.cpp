4a45c753fb1f66c14c007b96522905160762761e
dhananjay5005 <89375897+dhananjay5005@users.noreply.github.com>
2021-08-23 06:21:42 +0000

    new

---
+++ mul.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int firstNumber, secondNumber, multOfTwoNumbers;
+    
+    cout << "Enter two integers: ";
+    cin >> firstNumber >> secondNumber;
+
+    // sum of two numbers in stored in variable sumOfTwoNumbers
+    multOfTwoNumbers = firstNumber * secondNumber;
+
+    // Prints sum 
+    cout << firstNumber << " * " <<  secondNumber << " = " << multOfTwoNumbers;     
+
+    return 0;
+}
