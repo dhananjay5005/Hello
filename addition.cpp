dbe93df84927a4c7022bfa00b0284e6c6891d6a4
Dhananjay Nimbalkar <89375897+dhananjay5005@users.noreply.github.com>
2021-12-02 09:31:28 -0800

    Update addition.cpp

--- addition.cpp
+++ addition.cpp
@@ -8,10 +8,8 @@ int main()
     cout << "Enter two integers: ";
     cin >> firstNumber >> secondNumber;
 
-    // sum of two numbers in stored in variable sumOfTwoNumbers
     sumOfTwoNumbers = firstNumber + secondNumber;
 
-    // Prints sum 
     cout << firstNumber << " + " <<  secondNumber << " = " << sumOfTwoNumbers;     
 
     return 0;
